eb406808244c6c27caf97bba2b18283a3fd801a8
SunGC <1158984036>
2023-01-29 15:37:40 +0800

    feat: 添加element-plus组件自动导入配置

--- biliplayerPlus-2.0/vite.config.js
+++ biliplayerPlus-2.0/vite.config.js
@@ -3,6 +3,8 @@ import vue from '@vitejs/plugin-vue';
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import Icons from 'unplugin-icons/vite'
+import IconsResolver from 'unplugin-icons/resolver'
 import monkey, { cdn } from 'vite-plugin-monkey';
 import userscript from "./userscript";
 import path from "path";
@@ -19,20 +21,37 @@ export default defineConfig({
 	plugins: [
 		vue(),
 		monkey({
-		entry: 'src/main.js',
-		userscript: userscript,
-		build: {
-			fileName:userscript.version+".user.js",
-			externalGlobals: {
-			vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+			entry: 'src/main.js',
+			userscript: userscript,
+			build: {
+				fileName: userscript.version + ".user.js",
+				externalGlobals: {
+					vue: cdn.jsdelivr('Vue', 'dist/vue.global.prod.js'),
+				},
 			},
-		},
 		}),
 		AutoImport({
-			resolvers: [ElementPlusResolver()],
+			// 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+			resolvers: [
+				ElementPlusResolver(),
+				// 自动导入图标组件
+				IconsResolver({
+					prefix: 'Icon',
+				}),
+			],
 		}),
 		Components({
-			resolvers: [ElementPlusResolver()],
+			resolvers: [
+				// 自动注册图标组件
+				IconsResolver({
+					enabledCollections: ['ep'],
+				}),
+				// 自动导入 Element Plus 组件
+				ElementPlusResolver(),
+			],
+		}),
+		Icons({
+			autoInstall: true,
 		}),
 	],
 });
